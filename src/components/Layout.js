0bf107dba7cdc8492e47857e43c0b3336aae4102
trycrmr <trycrmr@gmail.com>
2020-08-17 13:10:21 -0400

    Adds back sass and inits some modular css

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import Helmet from 'react-helmet'
-
+import styles from '../styles/layout.module.scss'
 import Navbar from './Navbar'
 
 const TemplateWrapper = ({ children }) => (
-  <div>
+  <div className={styles.layout}>
     <Helmet title="Home | Gatsby + WordPress" />
     <Navbar />
     <div>{children}</div>
