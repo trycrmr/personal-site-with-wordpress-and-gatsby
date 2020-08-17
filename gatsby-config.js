229feb059a384bcb84c2f954c06b24d40d3bf4b0
trycrmr <trycrmr@gmail.com>
2020-08-17 12:37:42 -0400

    Corrects some mistaken edits to the config

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,16 +7,11 @@ module.exports = {
     {
       resolve: 'gatsby-source-wordpress',
       options: {
-        // The base url to your WP site.
-        baseUrl: 'cms.terrycreamer.codes',
-        // WP.com sites set to true, WP.org set to false
-        hostingWPCOM: false,
-        // The protocol. This can be http or https.
-        protocol: 'https',
-        // Use 'Advanced Custom Fields' Wordpress plugin
-        useACF: false,
-        auth: {},
-        // Set to true to debug endpoints on 'gatsby build'
+        baseUrl: 'cms.terrycreamer.codes', // The base url to your WP site.
+        hostingWPCOM: false, // WP.com sites set to true, WP.org set to false
+        protocol: 'https', // The protocol. This can be http or https.
+        useACF: false, // Use 'Advanced Custom Fields' Wordpress plugin
+        auth: {}, // Set to true to debug endpoints on 'gatsby build'
         verboseOutput: true,
       },
     },
