e181becb941ba398f290e5354d045c1f216c7aa3
trycrmr <trycrmr@gmail.com>
2020-08-22 19:28:24 -0400

    An attempt to add tests to bit. Doesn't work; Opened an issue.

---
+++ src/components/blog/PostCode/PostCode.test.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+// import PostTitleSection from './index.js'
+
+describe('PostCode', () => {
+  it('renders correctly', () => {
+    return true
+  })
+})
