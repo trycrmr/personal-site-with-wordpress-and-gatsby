67dbde8c7ccdaee3ca0681f834a0f50b5a8f8533
trycrmr <trycrmr@gmail.com>
2020-08-30 09:04:31 -0400

    Initial generic wordpress page styling

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,39 +1,68 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { graphql } from 'gatsby'
-import Layout from '../components/layout/Layout/Layout'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { graphql } from 'gatsby';
+import Layout from '../components/layout/Layout/Layout';
+import Heading from 'react-bulma-components/lib/components/heading';
+import Section from 'react-bulma-components/lib/components/section';
+import Container from 'react-bulma-components/lib/components/container';
+import Content from 'react-bulma-components/lib/components/content';
 
 export const PageTemplate = ({ title, content }) => {
   return (
     <section>
       <div>
-        <h2>{title}</h2>
-        <div dangerouslySetInnerHTML={{ __html: content }} />
+        <Section
+          size="medium"
+          style={{
+            maxHeight: '70vh',
+            minHeight: '400px',
+            textAlign: 'center',
+          }}
+        >
+          <Container>
+            <Heading
+              size={1}
+              weight="bold"
+              style={{
+                color: 'black',
+                textShadow:
+                  '1px 1px 6px whitesmoke, -1px 1px 6px whitesmoke, 1px -1px 6px whitesmoke, -1px -1px 6px whitesmoke',
+              }}
+            >
+              {title}
+            </Heading>
+          </Container>
+        </Section>
+        <Section>
+          <Content>
+            <div dangerouslySetInnerHTML={{ __html: content }} />
+          </Content>
+        </Section>
       </div>
     </section>
-  )
-}
+  );
+};
 
 PageTemplate.propTypes = {
   title: PropTypes.string.isRequired,
   content: PropTypes.string,
-}
+};
 
 const Page = ({ data }) => {
-  const { wordpressPage: page } = data
+  const { wordpressPage: page } = data;
 
   return (
     <Layout>
       <PageTemplate title={page.title} content={page.content} />
     </Layout>
-  )
-}
+  );
+};
 
 Page.propTypes = {
   data: PropTypes.object.isRequired,
-}
+};
 
-export default Page
+export default Page;
 
 export const pageQuery = graphql`
   query PageById($id: String!) {
@@ -42,4 +71,4 @@ export const pageQuery = graphql`
       content
     }
   }
-`
+`;
