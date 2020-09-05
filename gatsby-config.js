f24e116fa2086d5eea4f2ef27606c6f6e87d6c5b
trycrmr <trycrmr@gmail.com>
2020-09-05 09:31:02 -0400

    Adds google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,6 +23,17 @@ module.exports = {
         verboseOutput: `${process.env.PERSONAL_SITE_VERBOSE_OUTPUT}`,
       },
     },
+    {
+      resolve: 'gatsby-plugin-gtag',
+      options: {
+        // your google analytics tracking id
+        trackingId: `UA-31386246-6`,
+        // Puts tracking script in the head instead of the body
+        head: true,
+        // enable ip anonymization
+        anonymize: true,
+      },
+    },
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
     'gatsby-plugin-netlify', // make sure to keep it last in the array
