9f5b9f3413f66cfd1ffbdf2ba98094169e210aac
trycrmr <trycrmr@gmail.com>
2020-09-05 13:55:37 -0400

    Updates 404 page with guidance

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,26 @@
-import React from 'react'
-import Layout from '../components/layout/Layout/Layout'
+import React from 'react';
+import Layout from '../components/layout/Layout/Layout';
+import { Link } from 'gatsby';
+import Section from 'react-bulma-components/lib/components/section';
 
 const NotFoundPage = () => (
   <Layout>
     <div>
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <Section className="only-text-page" style={{ fontSize: '5vmin' }}>
+        <p>
+          Sorry to say, no page exists at this url. If you think something
+          should be here, <Link to={`/contact`}>contact me</Link>. Otherwise,
+          why not visit the <Link to={`/`}>homepage</Link>, collect your
+          bearings, and see if the content you seek can be accessed from there?
+        </p>{' '}
+        <br />
+        <p>
+          Regardless, happy to host you virtually; Come back anytime, and
+          remember to enjoy life and love yourself.
+        </p>
+      </Section>
     </div>
   </Layout>
-)
+);
 
-export default NotFoundPage
+export default NotFoundPage;
