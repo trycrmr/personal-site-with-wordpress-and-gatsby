284036ea1eddeb897b4e0de37d85279c6584bee5
trycrmr <trycrmr@gmail.com>
2020-09-12 16:18:39 -0400

    Removes ununsed dependency in the Banner

--- src/components/layout/Banner/index.js
+++ src/components/layout/Banner/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Container from 'react-bulma-components/lib/components/container';
 import Heading from 'react-bulma-components/lib/components/heading';
 import styles from './Banner.module.scss';
